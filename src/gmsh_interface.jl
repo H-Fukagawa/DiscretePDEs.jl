e95da5b6bf7771f8be1c18ae5b162c4c60d0d334
Hiroki Fukagawa <107348546+H-Fukagawa@users.noreply.github.com>
2024-12-22 03:18:28 +0900

    Update gmsh_interface.jl

--- src/gmsh_interface.jl
+++ src/gmsh_interface.jl
@@ -2,8 +2,8 @@ using DiscreteExteriorCalculus: Point, Simplex, TriangulatedComplex
 using UniqueVectors: UniqueVector
 using DotEnv
 
-# env = joinpath(@__DIR__, ".env")
-# DotEnv.load(env)
+env = joinpath(@__DIR__, ".env")
+DotEnv.load(env)
 include(ENV["gmshjl"]) # include the gmsh module used below
 
 export initialize!
