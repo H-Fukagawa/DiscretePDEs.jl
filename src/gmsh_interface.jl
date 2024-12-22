91b5aa5c0214fb43af09e7742ab0823dc984aefb
Hiroki Fukagawa <107348546+H-Fukagawa@users.noreply.github.com>
2024-12-22 11:29:42 +0900

    Update gmsh_interface.jl

--- src/gmsh_interface.jl
+++ src/gmsh_interface.jl
@@ -3,6 +3,7 @@ using UniqueVectors: UniqueVector
 using DotEnv
 
 env = joinpath(@__DIR__, ".env")
+open(env, "a") do io
 DotEnv.load(env)
 include(ENV["gmshjl"]) # include the gmsh module used below
 
